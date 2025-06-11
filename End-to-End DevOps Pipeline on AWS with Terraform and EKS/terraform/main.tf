5a41b064a6cc2775edde1a5d03722eda0c3d29b5
vatsan01 <66201006+vatsan01@users.noreply.github.com>
2025-06-12 03:56:36 +0530

    Update main.tf

--- End-to-End DevOps Pipeline on AWS with Terraform and EKS/terraform/main.tf
+++ End-to-End DevOps Pipeline on AWS with Terraform and EKS/terraform/main.tf
@@ -4,16 +4,17 @@ provider "aws" {
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "5.5.1"
+  version = "5.1.1"
 
-  name = "eks-vpc"
+  name = "devops-vpc"
   cidr = "10.0.0.0/16"
 
   azs             = ["us-east-1a", "us-east-1b"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
-  enable_nat_gateway = false
+  enable_nat_gateway = true
+  single_nat_gateway = true
 
   tags = {
     Project     = "devops-pipeline"
@@ -22,8 +23,8 @@ module "vpc" {
 }
 
 module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
-  version         = "20.8.3"
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.8.3"
 
   cluster_name    = "devops-cluster"
   cluster_version = "1.29"
@@ -31,12 +32,14 @@ module "eks" {
   subnet_ids = module.vpc.private_subnets
   vpc_id     = module.vpc.vpc_id
 
+  enable_irsa = true
+
   eks_managed_node_groups = {
     default = {
       desired_size   = 1
       max_size       = 1
       min_size       = 1
-      instance_types = ["t3.micro"] # Free tier
+      instance_types = ["t3.small"]
     }
   }
 
