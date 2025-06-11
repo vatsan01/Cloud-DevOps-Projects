bc4c2d989282550f94d81ec06a44e2c183c2c817
vatsan01 <66201006+vatsan01@users.noreply.github.com>
2025-06-12 01:30:48 +0530

    Update main.tf

--- End-to-End DevOps Pipeline on AWS with Terraform and EKS/terraform/main.tf
+++ End-to-End DevOps Pipeline on AWS with Terraform and EKS/terraform/main.tf
@@ -1,14 +1,47 @@
-
-# Terraform configuration for AWS EKS cluster
 provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.5.1"
+
+  name = "eks-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_nat_gateway = false
+
   tags = {
-    Name = "main-vpc"
+    Project     = "devops-pipeline"
+    Environment = "dev"
   }
 }
 
-# (Add EKS, Subnets, IAM roles, etc. here)
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  version         = "20.8.3"
+
+  cluster_name    = "devops-cluster"
+  cluster_version = "1.29"
+
+  subnet_ids = module.vpc.private_subnets
+  vpc_id     = module.vpc.vpc_id
+
+  eks_managed_node_groups = {
+    default = {
+      desired_size   = 1
+      max_size       = 1
+      min_size       = 1
+      instance_types = ["t3.micro"] # Free tier
+    }
+  }
+
+  tags = {
+    Project     = "devops-pipeline"
+    Environment = "dev"
+  }
+}
