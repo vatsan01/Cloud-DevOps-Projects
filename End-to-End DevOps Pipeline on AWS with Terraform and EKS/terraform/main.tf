cdc119caf5e78a890d06af7fef329e6ef43d26cf
vatsan01 <66201006+vatsan01@users.noreply.github.com>
2025-06-12 05:17:59 +0530

    Update main.tf

--- End-to-End DevOps Pipeline on AWS with Terraform and EKS/terraform/main.tf
+++ End-to-End DevOps Pipeline on AWS with Terraform and EKS/terraform/main.tf
@@ -23,23 +23,20 @@ module "vpc" {
 }
 
 module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "20.8.3"
-
+  source          = "terraform-aws-modules/eks/aws"
   cluster_name    = "devops-cluster"
   cluster_version = "1.29"
+  subnet_ids      = module.vpc.public_subnets
+  vpc_id          = module.vpc.vpc_id
 
-  subnet_ids = module.vpc.private_subnets
-  vpc_id     = module.vpc.vpc_id
-
-  enable_irsa = true
+  enable_cluster_endpoint_public_access = true
 
   eks_managed_node_groups = {
     default = {
       desired_size   = 1
       max_size       = 1
       min_size       = 1
-      instance_types = ["t3.small"]
+      instance_types = ["t3.micro"] # free-tier
     }
   }
 
