445ec916694afb6ea9cb2380c446ae98f7ed90dd
vatsan01 <66201006+vatsan01@users.noreply.github.com>
2025-06-13 06:09:11 +0530

    Update vpc.tf

--- Multi_AZ_Web_App_Deployment_with_High_Availability/terraform/vpc.tf
+++ Multi_AZ_Web_App_Deployment_with_High_Availability/terraform/vpc.tf
@@ -1,3 +1,4 @@
+# VPC Definition
 resource "aws_vpc" "main" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
@@ -6,4 +7,47 @@ resource "aws_vpc" "main" {
   tags = {
     Name = "main-vpc"
   }
+}
+
+# Public Subnets in two AZs
+resource "aws_subnet" "public_subnet" {
+  count                   = 2
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
+  map_public_ip_on_launch = true
+  availability_zone       = element(["us-east-1a", "us-east-1b"], count.index)
+
+  tags = {
+    Name = "public-subnet-${count.index}"
+  }
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main-igw"
+  }
+}
+
+# Public Route Table
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "public-route-table"
+  }
+}
+
+# Route Table Associations with Public Subnets
+resource "aws_route_table_association" "public_rt_assoc" {
+  count          = 2
+  subnet_id      = aws_subnet.public_subnet[count.index].id
+  route_table_id = aws_route_table.public_rt.id
 }
