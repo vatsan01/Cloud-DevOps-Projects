3498c0f06d51cc5e07705702540bf01663d76927
vatsan01 <66201006+vatsan01@users.noreply.github.com>
2025-06-13 06:59:44 +0530

    Update alb.tf

--- Multi_AZ_Web_App_Deployment_with_High_Availability/terraform/alb.tf
+++ Multi_AZ_Web_App_Deployment_with_High_Availability/terraform/alb.tf
@@ -33,3 +33,37 @@ resource "aws_lb" "app_lb" {
     Name = "app-lb"
   }
 }
+
+resource "aws_lb_target_group" "web_tg" {
+  name        = "web-tg"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.main.id
+  target_type = "instance"
+
+  health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    matcher             = "200-399"
+  }
+}
+
+resource "aws_lb_listener" "web_listener" {
+  load_balancer_arn = aws_lb.app_lb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.web_tg.arn
+  }
+}
+
+resource "aws_lb_target_group_attachment" "web_attachment" {
+  target_group_arn = aws_lb_target_group.web_tg.arn
+  target_id        = aws_instance.web.id
+  port             = 80
+}
