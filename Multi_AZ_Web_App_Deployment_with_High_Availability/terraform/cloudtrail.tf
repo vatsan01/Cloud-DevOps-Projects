a0899099a3a410c6f33def242f2764f7f5bf00b2
vatsan01 <66201006+vatsan01@users.noreply.github.com>
2025-06-13 06:36:11 +0530

    Update cloudtrail.tf

--- Multi_AZ_Web_App_Deployment_with_High_Availability/terraform/cloudtrail.tf
+++ Multi_AZ_Web_App_Deployment_with_High_Availability/terraform/cloudtrail.tf
@@ -1,16 +1,30 @@
-resource "random_id" "bucket_id" {
-  byte_length = 4
+resource "aws_s3_bucket_policy" "trail_bucket_policy" {
+  bucket = aws_s3_bucket.trail_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "cloudtrail.amazonaws.com"
+      },
+      Action = "s3:GetBucketAcl",
+      Resource = "arn:aws:s3:::${aws_s3_bucket.trail_bucket.id}"
+    },
+    {
+      Effect = "Allow",
+      Principal = {
+        Service = "cloudtrail.amazonaws.com"
+      },
+      Action = "s3:PutObject",
+      Resource = "arn:aws:s3:::${aws_s3_bucket.trail_bucket.id}/AWSLogs/${data.aws_caller_identity.current.account_id}/*",
+      Condition = {
+        StringEquals = {
+          "s3:x-amz-acl" = "bucket-owner-full-control"
+        }
+      }
+    }]
+  })
 }
 
-resource "aws_s3_bucket" "trail_bucket" {
-  bucket        = "srivatsan-cloudtrail-logs-${random_id.bucket_id.hex}"
-  force_destroy = true
-}
-
-resource "aws_cloudtrail" "trail" {
-  name                          = "example-trail"
-  s3_bucket_name                = aws_s3_bucket.trail_bucket.id
-  include_global_service_events = true
-  is_multi_region_trail         = true
-  enable_logging                = true
-}
+data "aws_caller_identity" "current" {}
