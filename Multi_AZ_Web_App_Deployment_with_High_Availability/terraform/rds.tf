184e332c84fc8f20a5f3d26b4bca8d096e1d74bc
vatsan01 <66201006+vatsan01@users.noreply.github.com>
2025-06-13 06:19:39 +0530

    Update rds.tf

--- Multi_AZ_Web_App_Deployment_with_High_Availability/terraform/rds.tf
+++ Multi_AZ_Web_App_Deployment_with_High_Availability/terraform/rds.tf
@@ -1,11 +1,11 @@
 resource "aws_db_instance" "default" {
   allocated_storage    = 20
   engine               = "mysql"
-  instance_class       = "db.t2.micro"
-  name                 = "mydb"
+  engine_version       = "8.0"
+  instance_class       = "db.t3.micro"
+  db_name              = "mydb"
   username             = "admin"
   password             = "password123"
-  parameter_group_name = "default.mysql8.0"
   skip_final_snapshot  = true
   multi_az             = true
 
