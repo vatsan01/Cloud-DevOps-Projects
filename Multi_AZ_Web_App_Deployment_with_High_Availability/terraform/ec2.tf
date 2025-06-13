53c62dbf42161fe3fb31d980b07f5363d0f53754
vatsan01 <66201006+vatsan01@users.noreply.github.com>
2025-06-13 06:19:16 +0530

    Update ec2.tf

--- Multi_AZ_Web_App_Deployment_with_High_Availability/terraform/ec2.tf
+++ Multi_AZ_Web_App_Deployment_with_High_Availability/terraform/ec2.tf
@@ -2,7 +2,14 @@ resource "aws_instance" "web" {
   ami           = var.ami_id
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.public_subnet[0].id
-  user_data     = file("app/index.html")
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum install -y httpd
+              systemctl start httpd
+              systemctl enable httpd
+              echo "<h1>Welcome to Highly Available Web App!</h1>" > /var/www/html/index.html
+              EOF
 
   tags = {
     Name = "web-instance"
