4d4c35250d01b2844fc0101ada76c52a22ced404
vatsan01 <66201006+vatsan01@users.noreply.github.com>
2025-06-13 06:18:46 +0530

    Update alb.tf

--- Multi_AZ_Web_App_Deployment_with_High_Availability/terraform/alb.tf
+++ Multi_AZ_Web_App_Deployment_with_High_Availability/terraform/alb.tf
@@ -1,3 +1,27 @@
+resource "aws_security_group" "lb_sg" {
+  name        = "alb-sg"
+  description = "Allow HTTP"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "alb-sg"
+  }
+}
+
 resource "aws_lb" "app_lb" {
   name               = "app-lb"
   internal           = false
