409ba5bfcf02792015e23600828a8a722d30893d
vatsan01 <66201006+vatsan01@users.noreply.github.com>
2025-06-13 07:07:14 +0530

    Update ec2.tf

--- Multi_AZ_Web_App_Deployment_with_High_Availability/terraform/ec2.tf
+++ Multi_AZ_Web_App_Deployment_with_High_Availability/terraform/ec2.tf
@@ -15,3 +15,26 @@ resource "aws_instance" "web" {
     Name = "web-instance"
   }
 }
+resource "aws_security_group" "web_sg" {
+  name        = "web-sg"
+  description = "Allow HTTP from ALB"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.lb_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "web-sg"
+  }
+}
