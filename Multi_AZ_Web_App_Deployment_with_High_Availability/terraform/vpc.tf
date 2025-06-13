0166ae807cc01bb909be355251a6c7213ce20697
vatsan01 <66201006+vatsan01@users.noreply.github.com>
2025-06-13 06:34:13 +0530

    Update vpc.tf

--- Multi_AZ_Web_App_Deployment_with_High_Availability/terraform/vpc.tf
+++ Multi_AZ_Web_App_Deployment_with_High_Availability/terraform/vpc.tf
@@ -19,3 +19,30 @@ resource "aws_subnet" "public_subnet" {
     Name = "public-subnet-${count.index}"
   }
 }
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main-igw"
+  }
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "public-rt"
+  }
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  count          = 2
+  subnet_id      = aws_subnet.public_subnet[count.index].id
+  route_table_id = aws_route_table.public_rt.id
+}
