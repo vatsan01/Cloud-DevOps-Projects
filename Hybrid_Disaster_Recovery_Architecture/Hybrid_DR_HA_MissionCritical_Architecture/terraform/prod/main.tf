575c2009305095eb94e10ec9f2aabe052672d0d5
Srivatsan G <srivatsang@Srivatsans-MacBook-Pro.local>
2025-06-22 14:16:03 +0530

    Cleaned up repo and moved files from subfolder to root

--- Hybrid_Disaster_Recovery_Architecture/Hybrid_DR_HA_MissionCritical_Architecture/terraform/prod/main.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
-module "vpc" {
-  source = "../modules/vpc"
-}
-
-# Other modules like ec2, rds would follow here
--- Hybrid_Disaster_Recovery_Architecture/Hybrid_DR_HA_MissionCritical_Architecture/terraform/prod/variables.tf
+++
@@ -1,6 +0,0 @@
-variable "subnet_id" {}
-variable "key_name" {}
-variable "db_username" {}
-variable "db_password" {}
-variable "security_group_id" {}
-variable "db_subnet_group" {}
